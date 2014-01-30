38db2b43cc7cbf73bac900283c010e1ccd87efef
Initscri <initscri@gmail.com>
2014-01-30 01:14:17 -0700

    create index.php add all commands

--- Upload/index.php
+++ Upload/index.php
@@ -1,1 +1,20 @@
 <?php
+/* Copyright 2013-2014 CoinOH.com PHP Bitcoin/Alt-Coin Faucet
+ * Developer : Initscri (github.com/initscri)
+ * Under: Coinoh.com (dev.coinoh.com)
+ */
+// init and define
+session_start();
+define('ROOTDIR', __DIR__);
+// include startup
+include ROOTDIR . "includes/startup.php";
+// go!
+$page = $_GET['page'];
+$faucet = new Faucet;
+$faucet->usePage($page);
+$faucet->run();
+// unset
+unset($page);
+unset($faucet);
+// END
+?>
