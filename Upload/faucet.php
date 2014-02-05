aa61c732debb6902de277db855a1ee7348f1852d
Initscri <initscri@gmail.com>
2014-02-05 11:16:46 -0700

    finish faucet.php (may need edits later)

--- Upload/faucet.php
+++ Upload/faucet.php
@@ -1,15 +1,59 @@
 <?php
-// Faucet file
-// contains class
+/* Copyright 2013-2014 CoinOH.com PHP Bitcoin/Alt-Coin Faucet
+ * Developer : Initscri (github.com/initscri)
+ * Under: Coinoh.com (dev.coinoh.com)
+ * This File: faucet.php - Faucet Class for running the sites primary functions.
+ */
 
 class Faucet {
 
   private $config_file = "custom/config.php"; // in custom directory for ease-of-use
   
+  private $action;
+  private $page;
   private $config;
+  
+  private $final_html;
 
   public function __contruct() {
     $this->loadConfigFile();
+  }
+  
+  public function setAction($action) {
+    $this->action = $action;
+  }
+  
+  public function replaceHTML($replace, $with) {
+    $final_html = $this->final_html;
+    $final_html = str_replace($replace, $with, $final_html);
+    $this->final_html = $final_html;
+  }
+  
+  public function finish() {
+    // return final product html
+    return $this->final_html;
+  }
+  
+  public function parsePage() {
+    $page = $this->page;
+    $action = $this->action;
+    $root_directory = $this->getRootDirectory();
+    include $root_directory.$page;
+    // bind to final_html
+    if(isset($html)) {
+      $this->final_html = $html;
+      return true;
+    }
+  }
+  
+  public function setAndCheckPage($page='home') {
+    $config = $this->config;
+    $pages = $config['pages'];
+    if(isset($pages[$page])) {
+      // page confirmed and allowed
+      $this->page = $pages[$page];
+      $this->parsePage();
+    }
   }
   
   public function loadConfigFile() {
