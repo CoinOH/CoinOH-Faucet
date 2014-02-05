ad0b86f63b33798abc60e8a506960b803d2d0935
Initscri <initscri@gmail.com>
2014-02-05 10:45:59 -0700

    Update index.php

--- Upload/index.php
+++ Upload/index.php
@@ -4,4 +4,23 @@
  * Under: Coinoh.com (dev.coinoh.com)
  */
 // Start!
+session_start();
+// include main libary
+include('faucet.php');
+// get page
+$page = $_GET['page'];
+// create faucet
+$faucet = new Faucet;
+$faucet->setAndCheckPage($page);
+// get final html
+$final_html = $faucet->finish();
+// remove spacing
+$html = trim($final_html);
+// echo $html to page
+echo $html;
+// unset
+unset($html);
+unset($final_html);
+unset($faucet);
+unset($page);
 ?>
