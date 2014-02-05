5e7e9d77c61a512d76d749e44e023af962c4acc7
Initscri <initscri@gmail.com>
2014-02-05 10:42:28 -0700

    reset and restart

--- Upload/index.php
+++ Upload/index.php
@@ -3,18 +3,5 @@
  * Developer : Initscri (github.com/initscri)
  * Under: Coinoh.com (dev.coinoh.com)
  */
-// init and define
-session_start();
-define('ROOTDIR', __DIR__);
-// include startup
-include ROOTDIR . "includes/startup.php";
-// go!
-$page = $_GET['page'];
-$faucet = new Faucet;
-$faucet->usePage($page);
-$faucet->run();
-// unset
-unset($page);
-unset($faucet);
-// END
+// Start!
 ?>
