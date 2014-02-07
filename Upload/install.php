19fcbb8ebbde1a828d3bec5881abe8d31619c820
Initscri <initscri@gmail.com>
2014-02-06 19:43:42 -0500

    init install.php

---
+++ Upload/install.php
@@ -0,0 +1,23 @@
+<?php
+/* Copyright 2013-2014 CoinOH.com PHP Bitcoin/Alt-Coin Faucet
+ * Developer : Initscri (github.com/initscri)
+ * Under: Coinoh.com (dev.coinoh.com)
+ * This File: install.php - Installs the script to your host
+ */
+ 
+$action = $_REQUEST['action']; // get and post
+$method = $_SERVER['REQUEST_METHOD']; // GET or POST
+
+if (isset($action)) {
+
+}
+else
+{
+  // begin install
+  if ($method == "POST") {
+  
+  }
+  if ($method == "GET") {
+  
+  }
+}
