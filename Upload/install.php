350bfc5d3c29e16af9c8e325c36cb4945d8f6d6f
Initscri <initscri@gmail.com>
2014-02-06 20:48:43 -0500

    Update install.php

--- Upload/install.php
+++ Upload/install.php
@@ -5,19 +5,31 @@
  * This File: install.php - Installs the script to your host
  */
  
-$action = $_REQUEST['action']; // get and post
 $method = $_SERVER['REQUEST_METHOD']; // GET or POST
-
-if (isset($action)) {
-
-}
-else
-{
-  // begin install
-  if ($method == "POST") {
-  
-  }
-  if ($method == "GET") {
-  
-  }
-}
+// header and meta html first
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CoinOH Faucet Install</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="/static/css/reset.css" />
+    <link rel="stylesheet" type="text/css" href="/static/css/install.css" />
+    <script type="text/javascript" src="/static/javascript/install.js"></script>
+  </head>
+  <body>
+    <div class="container">
+      <div class="title">CoinOH Faucet Install</div>
+      <div class="seperator"></div><div class="clear"></div>
+      <div class="content">
+        <?php
+          if ($method == "POST") {
+            // install
+          }
+          if ($method == "GET") {
+            // show config form
+          }
+      </div>
+    </div>
+  </body>
+</html>
